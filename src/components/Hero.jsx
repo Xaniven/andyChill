b4490efeeba99cae756da3249a740165833ae60e
Xaniven <k.kelley5978@gmail.com>
2023-05-20 19:55:13 -0400

    finish hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import andyMain from "../assets/IMG-0193.png";
 
 export default function Hero() {
   return (
-    <section id='home' className='h-[90vh] max-w-[100vw] px-10 pt-8 relative snap-start'>
+    <section
+      id='home'
+      className='h-[90vh] max-w-[100vw] px-10 pt-8 relative snap-start grid place-items-center '
+    >
       <div className='absolute block top-0 left-0 w-[100%] overflow-hidden -z-10'>
         <svg
           className='relative block w-[100%] h-[100vh]'
@@ -14,26 +17,26 @@ export default function Hero() {
           <path d='M1200 120L0 0 0 0 1200 0 1200 120z' className='fill-[#95f80a]'></path>
         </svg>
       </div>
-      dsajyhgdsk Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad eos doloremque laborum
-      officia, alias, perspiciatis soluta culpa, optio explicabo possimus deleniti hic sint?
-      Expedita assumenda eum vero similique, nobis fugit quasi odio quo temporibus minus quos quae
-      ab adipisci corporis totam ea obcaecati tenetur excepturi pariatur. Nam ab fuga eligendi,
-      quidem nihil quas! Explicabo laboriosam facere dignissimos vitae, consequatur alias placeat
-      suscipit ea hic cum accusantium nulla aut dolore minus possimus doloremque, neque ratione nam
-      fuga quasi. Incidunt dolores consequuntur deserunt, provident minus laborum voluptates quae
-      voluptate sed eos beatae recusandae iure delectus ullam ipsa maxime esse facere. Laboriosam
-      repellat, obcaecati vero facere eum similique tempora quas commodi, fuga facilis aspernatur
-      eos amet quia aut illo veniam non asperiores earum laudantium. Reprehenderit libero ipsa atque
-      inventore magni voluptates cum sequi id aut! Ipsa omnis eaque natus, nostrum sunt accusantium
-      architecto esse quos perferendis nam tempore. Voluptas, culpa quis! Eaque, rerum fugit.
-      Inventore quaerat labore culpa minus mollitia enim ducimus quod dolore explicabo autem optio
-      laborum dignissimos illum porro veniam ad consequatur voluptatem, magnam aliquam, odio esse,
-      fuga neque. Officiis aperiam ab fuga alias dolore hic dolor, quaerat, aliquid culpa possimus
-      placeat eligendi ratione debitis similique autem! Aliquid commodi voluptatum ipsa voluptates
-      maxime neque quia praesentium ex! Itaque rem illum, perspiciatis tempore, maiores incidunt
-      optio, quis est alias excepturi nemo! Reiciendis hic mollitia commodi, debitis praesentium
-      corporis dolores nisi. Ex ad hic temporibus eos, saepe itaque distinctio in, fuga quas ipsam
-      deleniti qui, ullam suscipit magni molestiae delectus fugit modi quam.
+      <div className='heroContent flex h-[100%] max-w-[100vw]'>
+        <img
+          src={andyMain}
+          className=' drop-shadow-2xl h-[75%] shadow-2xl border-8 border-sky-500 absolute left-20 top-20'
+          alt=''
+        />
+        <div className='w-[60vw] h-fit rotate-[28deg] absolute top-[35vh] right-12'>
+          <h1 className=' text-5xl font-bold pb-2'>I am Andy Chill</h1>
+          <p className='text-2xl'>
+            Born into a colorful world in May of 2021, I immediately became tranfixed with you
+            humans and your self expression. One of you particularly entriged me, one Mr. Warhol. I
+            knew on sight, he was the one I would imitate and based myself on.
+          </p>
+        </div>
+        <div className=' absolute bottom-[25%] left-[33%] w-96 '>
+          “Art is anything you can get away with. -Andy Warhol”
+          <br />
+          -Andy Chill
+        </div>
+      </div>
     </section>
   );
 }
